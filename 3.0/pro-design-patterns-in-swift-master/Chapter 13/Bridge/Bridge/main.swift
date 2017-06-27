8b4d3f027e53e13d0135abea5a5de0cae5166ae1
lixin1988 <lixin@fkhwl.com>
2017-06-27 10:19:03 +0800

    remove dir

--- 3.0/pro-design-patterns-in-swift-master/Chapter 13/Bridge/Bridge/main.swift
+++
@@ -1,5 +0,0 @@
-var comms = Communicator(channel: Channel.Channels.satellite);
-
-comms.sendCleartextMessage("Hello!");
-comms.sendSecureMessage("This is a secret");
-comms.sendPriorityMessage("This is important");
